430b1f3ea06abfa602e312090dd6bb57355213ed
rllsh57 <r.ll.s.h57@gmail.com>
2015-05-25 13:42:04 +0300

    Рефакторинг MainActivity
    - Вырезан не используемый код.

--- src/by/bsuir/osisp/wifimap/MainActivity.java
+++ src/by/bsuir/osisp/wifimap/MainActivity.java
@@ -9,10 +9,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.dao.DaoManager;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.table.TableUtils;
 
 
 public class MainActivity extends Activity {
@@ -35,7 +31,6 @@ public class MainActivity extends Activity {
         WifiDatabaseManager manager = new WifiDatabaseManager();
         manager.connectToDatabase();
         manager.disconnectFromDatabase();
-//        run();
     }
     
     
@@ -43,38 +38,6 @@ public class MainActivity extends Activity {
 		Looper.prepare();
 		Toast.makeText(mInstance, text, Toast.LENGTH_LONG).show();
 		Looper.loop();
-    }
-    
-    
-    void run() {
-    	Thread thr = new Thread() {
-    		private JdbcConnectionSource mConnSource;
-    		Dao<WifiNetwork, Integer> mDao;
-    		
-    		public void run() {
-    			try {
-    				doJob();
-    			}
-    			catch (Exception ex) {
-    				makeToast(ex.toString());
-    			}
-    		}
-    			
-    		void doJob() throws Exception { 
-    			mConnSource = new JdbcConnectionSource("jdbc:mysql://192.168.100.2:3306/test_wifi_map", "rllsh57", "107295");
-    			mDao = DaoManager.createDao(mConnSource, WifiNetwork.class);
-//    			TableUtils.createTable(mConnSource, WifiNetwork.class);
-    			
-    			WifiNetwork ap = new WifiNetwork("rllsh57", "some pass");
-    			WifiNetwork ap2 = new WifiNetwork("lalka", "adwadawd");
-    			mDao.create(ap);
-    			mDao.create(ap2);
-    			
-    			mConnSource.close();
-    			makeToast("Okay");
-    		}
-    	};
-    	thr.start();
     }
 }
 
