077bee1c73fca812065deb3e617216582d175c35
rllsh57 <r.ll.s.h57@gmail.com>
2015-05-25 17:48:29 +0300

    Изменены маркеры. Скрытие маркеров во время zoomout.
    - Добавил title и snippet к маркерам на карте.
    - Маркеры скрываются при зуме меньше 16.0

--- src/by/bsuir/osisp/wifimap/GoogleMapManager.java
+++ src/by/bsuir/osisp/wifimap/GoogleMapManager.java
@@ -5,40 +5,55 @@ import java.util.List;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class GoogleMapManager {
+public class GoogleMapManager implements GoogleMap.OnCameraChangeListener {
 	
 	private GoogleMap mMap;
+	private List<WifiNetwork> mNetworks;
 
 	
 	public GoogleMapManager(MapFragment mapFragment) {
 		mMap = mapFragment.getMap();
-        mMap.setIndoorEnabled(true);
         mMap.setMyLocationEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
                 new LatLng(53.901152, 27.553153), 18));
+        mMap.setOnCameraChangeListener(this);
 	}
 	
 	
 	private void displayNetworksOnMap(List<WifiNetwork> networks) {
+		if (networks == null)
+			return;
 		for (WifiNetwork network: networks) {
 			MarkerOptions marker = new MarkerOptions()
-					.position(new LatLng(network.getLattitude(), network.getLongitude())
-			);
+					.position(new LatLng(network.getLattitude(), network.getLongitude()))
+					.title(network.getSsid())
+					.snippet(network.getPassword());
 			mMap.addMarker(marker);
 		}	
 	}
 	
 	
 	public void displayNetworks(final List<WifiNetwork> networks) {
+		mNetworks = networks;
 		MainActivity.runTask(new Runnable() {
 			@Override
 			public void run() {
 				displayNetworksOnMap(networks);
 			}
 		});
+	}
+
+
+	@Override
+	public void onCameraChange(CameraPosition position) {
+		if (position.zoom <= 16.0)
+			mMap.clear();
+		else
+			displayNetworksOnMap(mNetworks);
 	}
 }
