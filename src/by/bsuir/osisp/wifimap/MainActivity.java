076807a0ac6f2e13ee9c583ac1ec5810ef689270
rllsh57 <r.ll.s.h57@gmail.com>
2015-05-31 00:46:01 +0300

    Добавил выбор темы (светлая или темная)

--- src/by/bsuir/osisp/wifimap/MainActivity.java
+++ src/by/bsuir/osisp/wifimap/MainActivity.java
@@ -3,6 +3,7 @@ package by.bsuir.osisp.wifimap;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -11,14 +12,13 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
-
-	public static SharedPreferences sharedPrefences;
+public class MainActivity extends Activity implements OnSharedPreferenceChangeListener {
 
 	private static MainActivity mInstance;
+	private SharedPreferences mSharedPrefences;
 	private GoogleMapManager mMapManager;
 	private WifiDatabaseManager mDbManager;
-	
+		
 	
     public static void makeToast(final String text) {
     	mInstance.runOnUiThread(new Runnable() {
@@ -33,15 +33,25 @@ public class MainActivity extends Activity {
 	public static void runTask(Runnable task) {
 		mInstance.runOnUiThread(task);
 	}
-    
-    
+
+	
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals(SettingsActivity.KEY_PREF_THEME))
+			recreate();
+	}
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	mInstance = this;
-		sharedPrefences = PreferenceManager.getDefaultSharedPreferences(this);
-    	
+
+    	mSharedPrefences = PreferenceManager.getDefaultSharedPreferences(this);
+    	mSharedPrefences.registerOnSharedPreferenceChangeListener(this);
+
     	super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+		setTheme(Integer.valueOf(mSharedPrefences.getString(SettingsActivity.KEY_PREF_THEME, "")));
+    	setContentView(R.layout.activity_main);
 
     	mMapManager = new GoogleMapManager(this);
     	mDbManager = new WifiDatabaseManager(mMapManager);
--- src/by/bsuir/osisp/wifimap/SettingsActivity.java
+++ src/by/bsuir/osisp/wifimap/SettingsActivity.java
@@ -1,17 +1,34 @@
 package by.bsuir.osisp.wifimap;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
 	public static final String KEY_PREF_DATABASE_SERVER = "pref_db_server";
-	
+	public static final String KEY_PREF_THEME = "pref_theme";
+
+	private SharedPreferences mSharedPrefences;
 	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {    	
+    	mSharedPrefences = PreferenceManager.getDefaultSharedPreferences(this);
+    	mSharedPrefences.registerOnSharedPreferenceChangeListener(this);
+
+    	setTheme(Integer.valueOf(mSharedPrefences.getString(KEY_PREF_THEME, "")));
+    	
     	super.onCreate(savedInstanceState);
     	addPreferencesFromResource(R.xml.preferences);
     }
+
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+		if (key.equals(SettingsActivity.KEY_PREF_THEME))
+			recreate();
+	}
 }
